62d193dfb7c8ab6f13ef6c3b8a88797619a0ba0b
sergey <ksvomega@gmail.com>
2019-12-01 21:29:26 +0200

    Added whole project2

--- src/main/Java/com/kozinets/orders2/OrdersStarter.java
+++ src/main/Java/com/kozinets/orders2/OrdersStarter.java
@@ -1,19 +1,16 @@
 package com.kozinets.orders2;
 
-import org.springframework.boot.SpringApplication;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.SpringApplication;
+        import org.apache.logging.log4j.LogManager;
+        import org.apache.logging.log4j.Logger;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrdersStarter {
     //@Autowired
     //FillDataFirst fillDataFirst;
 
-    private static Logger LOGGER = LogManager.getLogger(OrdersStarter.class);
-
     public static void main(String[] args){
-        LOGGER.info("Simple log statement with inputs 1, 2 and 3");
         SpringApplication.run(OrdersStarter.class, args);
         FillerDataFirst.iteration();
     }
